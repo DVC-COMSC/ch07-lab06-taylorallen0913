c5e4f8133e8da2f05b0e5cdb906899788c6f911a
Taylor Allen <taylorallen0913@gmail.com>
2022-11-01 17:16:00 -0700

    test

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 inputvalues = input('Enter all elements values: ')
 numbers1 = inputvalues.split() 
 numbers2 = []
+
 for i in range(len(numbers1)):
 	numbers1[i] = int(numbers1[i]) 
+
+print ("The original list: ", numbers1)
 
 for i in range(len(numbers1)):
 	numbers2.insert(i, numbers1.pop())
 
-print ("The original list: ", numbers1)
+
 # print(numbers2)
